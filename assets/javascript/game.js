13a15a13cfc9f1c951a46ccb310d8b4720e71644
Zac Morton <zmorton8057@gmail.com>
2019-06-15 21:32:05 -0700

    Counters work and reinitializes the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 ////Global variable
-var wins = [];
-var losses= [];
+var wins = 0;
+var losses= 0;
 var currentScore = 0;
 var scoreToGuess = [];
 var crystal1 = [];
@@ -8,15 +8,25 @@ var crystal2 = [];
 var crystal3 = []; 
 var crystal4 = [];
 
-var a = 0;
-var b = 0;
-  
-
-
-////randomNumber Function
+////randomNumber Global Function
 function randomNumber(min, max){
   return Math.round( Math.random() * (max - min) + min);
 }
+
+var resetVariables = function(){
+  currentScore = 0;
+  $('#yourScore').text(currentScore)
+  scoreToGuess = randomNumber(10,40);
+  $('#numberToGuess').text(scoreToGuess)
+  crystal1 = randomNumber(1,9);
+  crystal2 = randomNumber(1,9);
+  crystal3 = randomNumber(1,9); 
+  crystal4 = randomNumber(1,9);
+}
+
+///Prints wins and loss counter to the DOM
+$('#wins').text("Wins: " + wins)
+$('#losses').text("Losses: " + losses)
 
 /// creates crystal variables with randomNumber function
 crystal1 = randomNumber(1, 9)
@@ -32,27 +42,74 @@ console.log(crystal4)
 /// creates random number for user to try and reach
 scoreToGuess = randomNumber(10,40)
 $('#numberToGuess').text(scoreToGuess)
+console.log(scoreToGuess)
+
 
   // Assign each of the values to a button, logs the value of the crystal to currentScore returns new value of currentScore
 $('#crystal1').click(function(){
    currentScore = (crystal1 + currentScore);
-    $('#yourScore').text("Total: "+ currentScore);        
+    $('#yourScore').text("Total: "+ currentScore);  
+    if(currentScore === scoreToGuess){
+      alert("You Win")
+      wins++
+      $('#wins').text("Wins: " + wins)
+      resetVariables()
+    } else if(currentScore > scoreToGuess){
+      alert("You Lose")
+      losses++
+      $('#losses').text("Losses: " + losses)
+      resetVariables()
+    }             
 });
 
 $('#crystal2').click(function(){
   currentScore = (crystal2 + currentScore);
-   $('#yourScore').text("Total: "+ currentScore);        
+   $('#yourScore').text("Total: "+ currentScore); 
+   if(currentScore === scoreToGuess){
+    alert("You Win")
+    wins++
+    $('#wins').text("Wins: " + wins)
+    resetVariables()
+  } else if(currentScore > scoreToGuess){
+    alert("You Lose")
+    losses++
+    $('#losses').text("Losses: " + losses)
+    resetVariables()
+  }              
 });
 
 $('#crystal3').click(function(){
   currentScore = (crystal3 + currentScore);
-   $('#yourScore').text("Total: "+ currentScore);        
+   $('#yourScore').text("Total: "+ currentScore);  
+   if(currentScore === scoreToGuess){
+    alert("You Win")
+    wins++
+    $('#wins').text("Wins: " + wins)
+    resetVariables()
+  } else if(currentScore > scoreToGuess){
+    alert("You Lose")
+    losses++
+    $('#losses').text("Losses: " + losses)
+    resetVariables()
+  }             
 });
 
 $('#crystal4').click(function(){
   currentScore = (crystal4 + currentScore);
-   $('#yourScore').text("Total: "+ currentScore);        
+   $('#yourScore').text("Total: "+ currentScore); 
+   if(currentScore === scoreToGuess){
+     alert("You Win")
+     wins++
+     $('#wins').text("Wins: " + wins)
+     resetVariables()
+   }  else if(currentScore > scoreToGuess){
+     alert("You Lose")
+     losses++
+     $('#losses').text("Losses: " + losses)
+     resetVariables()
+   }    
 });
+
 
 
 
